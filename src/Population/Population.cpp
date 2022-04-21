7bc5addc51059dadde84cb980e13397939b72c37
Kaleb-Bruwer <kaleb.bruwer@gmail.com>
2022-04-21 11:07:08 +0200

    Accounted for floating point error

--- src/Population/Population.cpp
+++ src/Population/Population.cpp
@@ -19,13 +19,23 @@ void Population::rampedFull(int popSize, int maxDepth){
 
     int numDone = 0;
     for(int i=2; i<=maxDepth; i++){
-        int nextStop = (i-1)*(popSize/(double)(maxDepth - 1));
+        int nextStop = ((i-1)*popSize)/(double)(maxDepth - 1);
         for(; numDone < nextStop; numDone++){
             vector<Node> result = generateFullTree(i);
             filledSize += result.size();
             startIndexes.push_back(filledSize-1);
             treesVec.insert(treesVec.end(), result.begin(), result.end());
         }
+    }
+
+    // nextStop may sometimes be broken by a floating point error,
+    // this is to make sure the full amount of trees is always generated
+    while(numDone < popSize){
+        vector<Node> result = generateFullTree(maxDepth);
+        filledSize += result.size();
+        startIndexes.push_back(filledSize-1);
+        treesVec.insert(treesVec.end(), result.begin(), result.end());
+        numDone++;
     }
 
     trees = new Node[filledSize];
