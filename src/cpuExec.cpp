2ac5c06cb6c44e693ad7642478c26ce937f19a24
Kaleb-Bruwer <kaleb.bruwer@gmail.com>
2022-04-30 11:11:35 +0200

    Non-full tree compatible execution

--- src/cpuExec.cpp
+++ src/cpuExec.cpp
@@ -14,102 +14,87 @@ const int numFloats = 8;
 int execTree(Node* node, int* data, int r){
     stack<int> compStack;
 
-    while(*node != Node{0,0}){
-        // Go to 1st function node
-        while(node->type != 0)
-            node--;
+    for(;*node != Node{0,0}; node--){
 
-        int left = INT_MAX, right = INT_MAX;
-
-        // Get n parameters
-        int nParams = node->nParameters();
-        unsigned char filled = 0;
-
-        // Check for terminals
-        if(node[1].type != 0){
-            if(node[1].type == 2) //int
-                left = data[nRecords*(numFloats + node[1].val)+r];
-            else //float
-                left = data[nRecords*(node[1].val)+r];
-            filled++;
-
-            if(node[2].type != 0){
-                if(node[2].type == 2) //int
-                    right = data[nRecords*(numFloats + node[2].val)+r];
-                else //float
-                    right = data[nRecords*(node[2].val)+r];
-                filled++;
-            }
-        }
-        // If not enough terminals, take values from stack
-        if(filled < nParams){
-            if(filled == 0){
+        switch(node->type){
+            case 0:{ //function
+                int result;
+                int left, right;
+                // All functions have at least one input
                 left = compStack.top();
                 compStack.pop();
-                filled++;
-            }
-            if(filled < nParams){
-                right = compStack.top();
-                compStack.pop();
-            }
-        }
-
-        // calc result, put on stack
-        int result;
-        if(!(node[0].val & 32)){
-            switch(node[0].val){
-                case 1: //+
-                    result = left + right;
-                    break;
-                case 2: //-
-                    result = left - right;
-                    break;
-                case 3: //*
-                    result = left * right;
-                    break;
-                case 4: //divide
-                    if(right == 0) right = 1;
-                    result = left / right;
-                    break;
-                case 5: //mod
-                    if(right < 1) right = 1;
-                    result = left % right;
-                    break;
-                case 6: //round
-                    result = (int) castF(left);
-                    break;
-            }
-        }
-        else{
-            float fResult;
-            switch(node[0].val){
-                case 33: //+
-                    fResult = castF(left) + castF(right);
-                    break;
-                case 34: //-
-                    fResult = castF(left) - castF(right);
-                    break;
-                case 35: //*
-                    fResult = castF(left) * castF(right);
-                    break;
-                case 36:{ //divide
-                    float fRight = castF(right);
-                    if(fRight == 0)
-                        fRight = 1;
-                    fResult = castF(left) / fRight;
-                    break;
+                if(node->nParameters() == 2){
+                    right = compStack.top();
+                    compStack.pop();
                 }
-                case 37:
-                    fResult = left;
-                    break;
+
+                if(!(node[0].val & 32)){ //checks if it's an int or float function
+                    switch(node[0].val){
+                        case 1: //+
+                            result = left + right;
+                            break;
+                        case 2: //-
+                            result = left - right;
+                            break;
+                        case 3: //*
+                            result = left * right;
+                            break;
+                        case 4: //divide
+                            if(right == 0) right = 1;
+                            result = left / right;
+                            break;
+                        case 5: //mod
+                            if(right < 1) right = 1;
+                            result = left % right;
+                            break;
+                        case 6: //round
+                            result = (int) castF(left);
+                            break;
+                    }
+                }
+                else{
+                    float fResult;
+                    switch(node[0].val){
+                        case 33: //+
+                            fResult = castF(left) + castF(right);
+                            break;
+                        case 34: //-
+                            fResult = castF(left) - castF(right);
+                            break;
+                        case 35: //*
+                            fResult = castF(left) * castF(right);
+                            break;
+                        case 36:{ //divide
+                            float fRight = castF(right);
+                            if(fRight == 0)
+                                fRight = 1;
+                            fResult = castF(left) / fRight;
+                            break;
+                        }
+                        case 37:
+                            fResult = left;
+                            break;
+                    }
+                    result = castI(fResult);
+                }
+
+                compStack.push(result);
+                break;
             }
-            result = castI(fResult);
-        }
 
-        compStack.push(result);
-        node--;
-    }
-    // the only value on the stack is the answer
+            case 1: //const
+                compStack.push(node->val);
+                break;
+            case 2: //INT
+                compStack.push(data[nRecords*(numFloats + node->val)+r]);
+                break;
 
+            case 3: //FLOAT
+                compStack.push(data[nRecords*(node->val)+r]);
+                break;
+        }//switch
+    }//for
+
+    // the only value still on the stack is the answer
     return compStack.top();
 }
